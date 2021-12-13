0bbc7d4c60bfb8690c13a76925e8b8e7027ce8f8
Qianqian Song <wasqqdyx@gmail.com>
2021-12-13 10:21:45 -0500

    Update example.R

--- R/example.R
+++ R/example.R
@@ -1,11 +1,12 @@
 ## library(SMGR)
 
 #' load the example data
-## data("example1")
-## data("example1.member")
+## data("data1")
+## data("data1.member")
 
-## result1 <- smgr_main(datalist=example1, N=3, K=nrow(example1[[1]]))
+## result1 <- smgr_main(datalist=data1, N=3, K=nrow(data1[[1]]))
 
 #' calculate the ARI index
 ## library(clues)
-## adjustedRand(result1$clusters,example1.member)
+## adjustedRand(result1$clusters,data1.member)
+
