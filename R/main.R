b1eca5551865290ff652c77ba886a525651af7f0
Qianqian Song <wasqqdyx@gmail.com>
2022-04-13 00:59:45 -0400

    Add files via upload

---
+++ R/main.R
@@ -0,0 +1,154 @@
+
+#' @title required packages
+
+ipak <- function(pkg){
+    new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+    if (length(new.pkg)>0){
+        BiocManager::install(new.pkg, dependencies = TRUE)
+    } else {
+        sapply(pkg, require, character.only = TRUE)
+    }
+}
+
+#' @title SMGR main function
+#' @description extract co-regulation genes across single-cell multi-omics data
+#' @param sm.data consists of single-cell RNA-seq data and single-cell ATAC-seq data in the format of matrix. Columns represent cells and rows represent genes (scATAC-seq input has been converted to gene-based data matrix)
+#' @param K The number of co-regulation clusters in the single-cell multi-omics data
+#' @param R the number of rows in the data matrix
+#' @param n_burnin burnin for gibbs sampling
+#' @param n_draw drawing for gibbs sampling
+#' @param n_iter number of iterations
+#' @param option nb or zinb
+#' @param type simulate or experimental data
+#' @return latent representation; co-regulation clusters; BIC values
+#' @export smgr_main function
+
+smgr_main1 <- function(sm.data, K, R, n_burnin=200, n_draw=200, n_iter=20, option='nb',
+                      type='simulate', BIC=FALSE)
+{
+
+    cat("## ============================================================================\n")
+    cat("## |                             running SMGR                                  |\n")
+    cat("## ----------------------------------------------------------------------------\n")
+
+    len <- length(sm.data)
+    
+    sm.datas <- transform(sm.data, len, R, type) 
+    
+    if (len==2){
+        cat("## |            scRNA-seq data and scATAC-seq data identified ....              |\n")
+    } else if (len<2){
+          cat("## |               single-cell multi-omics data is not identified ....       |\n")}
+
+    cat("## |                           Set initial values ....                         |\n")
+    mean_X = matrix(0, nrow = K, ncol = R)
+    init_Y <- matrix(rnorm(K*R,0,1), nrow = K, ncol = R)
+    update_Y <- init_Y
+
+    iter = 0
+
+    while (iter < n_iter)
+    {
+        iter = iter + 1
+        
+  if (iter<10){term <- paste0('iter= ',iter)}else {term <- paste0('iter=',iter)}
+           cat(paste0('## |                               ',term,'.....                                |\n'))
+
+        if (option=='zinb'){
+
+            estimates <- mclapply(sm.datas, function(dat){
+                beta0 <- dat$b_0; beta1 <- dat$b_1
+
+                estimate_i <- mclapply(1:ncol(dat$x_i), function(f)
+                {
+                    datas <- data.frame(update_Y,y=dat$x_i[,f]);
+
+                    tmp <- mclapply(1:(ncol(datas)-1),function(l){
+                        m1 <- zeroinfl(y ~ ., dist = 'negbin',
+                                       data = datas[,c(l,ncol(datas),drop=FALSE)])
+                        return(m1)}, mc.cores = detectCores(), mc.preschedule=FALSE,mc.set.seed=FALSE)
+
+                    incept1 <- mean(sapply(tmp,function(m1){coef(m1)[1]}))
+                    incept2 <- as.numeric(sapply(tmp,function(m1){coef(m1)[2]}))
+                    zinb.coef <- as.matrix(c(incept1,incept2))
+                    zinb.th <- mean(sapply(tmp,function(m1){summary(m1)$theta}))
+                    zinb.mu <- as.numeric(sapply(tmp,function(m1){m1$fitted.values[which.min(m1$residuals)]}))
+                    temp <- list(zinb_coef = zinb.coef,
+                                 zinb_th = zinb.th,
+                                 zinb_mu = zinb.mu)
+                    return(temp)
+                }, mc.cores = detectCores(), mc.preschedule=FALSE,mc.set.seed=FALSE)
+                
+                dat$coef <- do.call(cbind, map(estimate_i, 1))
+                dat$th <- do.call(cbind, map(estimate_i, 2))
+                dat$mu <- do.call(cbind, map(estimate_i, 3))
+                dat$b0 <- dat$coef[1,]; dat$b1 <- t(dat$coef[-1,])
+                return (dat)
+            }, mc.cores = detectCores(), mc.preschedule=FALSE,mc.set.seed=FALSE)
+            }
+            
+        estimates <- mclapply(sm.datas, function(dat){
+            beta0 <- dat$b_0; beta1 <- dat$b_1
+            estimate_i <- mclapply(1:ncol(dat$x_i), function(f)
+                {
+                    datas <- data.frame(update_Y,y=dat$x_i[,f]);
+                    nb_reg <- glmregNB(y ~ ., data=datas, family='negbin', penalty="enet",
+                                       lambda =1, alpha=1, thresh=1e-4,
+                                       standardize=FALSE, parallel=FALSE);
+
+                    temp <- list(nb_coef = as.matrix(coef(nb_reg)),
+                                 nb_th = nb_reg$theta,
+                                 nb_mu = nb_reg$fitted.values)
+                    return(temp)}, mc.cores = detectCores(), mc.preschedule=FALSE,mc.set.seed=FALSE)
+
+            dat$coef <- do.call(cbind, map(estimate_i, 1))
+            dat$th <- do.call(cbind, map(estimate_i, 2))
+            dat$mu <- do.call(cbind, map(estimate_i, 3))
+            dat$b0 <- dat$coef[1,]; dat$b1 <- t(dat$coef[-1,])
+            return (dat)
+        }, mc.cores = detectCores(), mc.preschedule=FALSE,mc.set.seed=FALSE)
+        
+        tmp_update <- tryCatch(
+        {
+            update_Ys(mean_x = mean_X,
+                      update_y = update_Y,
+                      estimate_y = estimates,
+                      k = K,
+                      r = R,
+                      n = len,
+                      nburnin = n_burnin,
+                      ndraw = n_draw)
+        },
+        error=function(cond) {
+            message("\n ##  Errors occured during optimization... \n")
+            message("\n ## Here's the original error message... \n")
+            message(cond)
+            return(NA)
+        },
+        warning=function(cond) {
+                return(NULL)
+                }
+        )
+
+        update_Y <- tmp_update
+        }
+
+
+    kmeans.fit = kmeans(update_Y, K, nstart=100)
+    Yclusters = kmeans.fit$cluster
+
+    if (output =='all'){
+        totalBic <- sum(unlist(
+            mclapply(estimates, function(dx)
+        {
+            BICs(xi = dx$x_i, beta = dx$b1, th = dx$th[1,], mu = dx$mu, update = update_Y)
+        }, mc.cores = detectCores(), mc.preschedule = FALSE, mc.set.seed = FALSE)));
+    latents <- list(estimate_f = estimates, latent_f = update_Y, bic_f = totalBic)
+    } else {
+        latents <- list(latent_f = update_Y, clusters = Yclusters) }
+        
+    return (latents)
+
+    cat("## ======================= finished ===================================\n")
+}
+
---
+++ R/optimization.R
@@ -0,0 +1,113 @@
+
+#' @title data transformation
+#' @param datatype
+#' @export tranfromed data
+transform <- function(data, len, R, type){
+    if (len>0){
+        if (type=='simulate'){
+            datas <- data
+        } else if (type=='real') {
+            convert <- GeneActivityMatrix(peak.matrix = data[[2]],
+                                          annotation.file = "Homo_sapiens.GRCh37.82.gtf",
+                                          seq.levels = c(1:22, "X", "Y"),
+                                          upstream = 2000, verbose = TRUE)[[3]]
+            datas <- data; dats[[2]] <- convert }
+
+        dat <- lapply(datas,function(x){
+            meanx = apply(x, 2, mean);
+            B0 = log(meanx); B1=matrix(0, nrow = ncol(x), ncol = R)
+            list(x_i=x, mean_x=meanx, b_0=B0, b_1=B1, p = ncol(x))})
+
+        return (dat)
+    } else{
+        stop("Error: at least one dataset")
+    }
+}
+
+
+#' @title calculate the BIc value
+#' @description calculate bic
+#' @details calculate the BIc value to seletct the best latent variables
+#' @param xi
+#' @return the BIC value
+#' @export  BICs function
+
+BICs <- function(xi, beta, th, mu, update){
+  BIC = 0
+  all.ll <- sum(unlist(lapply(1:nrow(xi),function(i){
+    loglik(th,mu[i,],y=xi[i,])})))
+  BIC = BIC - 2*all.ll + (sum(beta!=0))*log(nrow(xi)*ncol(xi))
+  return (BIC)}
+
+#' @title calculate the loglikehood
+#' @description loglikehood
+#' @details calculate the loglikehood based on the negative binomial distribution
+#' @param th theta in the negative binomial distribution
+#' @param mu mu in the negative binomial distribution
+#' @return A numeric vector of log density.
+#' @export  posterior likelihood
+
+loglik <- function(th, mu, y){
+  sum(lgamma(th + y) - lgamma(th) - lgamma(y + 1) + th * log(th) +
+        y * log(mu + (y == 0)) - (th + y) * log(th + mu))
+}
+
+
+#' @title Gibbs sampling for identifying latent representation
+#' @param mean_x A non-negative integer. or vector
+#' @return updated latent representation
+#' @export  update_Ys function
+update_Ys <- function(mean_x,
+                      update_y,
+                      estimate_y,
+                      k,
+                      r,
+                      n,
+                      sdev=0.05,
+                      nburnin,
+                      ndraw) {
+
+    a = vector('list')
+    b = vector('list')
+    x <- lapply(1:2,function(i){matrix(0, nrow = k, ncol = r)})
+    mu <- lapply(1:2,function(i){matrix(0, nrow = k, ncol = r)})
+    th <- lapply(1:2,function(i){matrix(0, nrow = k, ncol = r)})
+    p = rep(1:2)
+
+    for ( j in 1:n){
+        a[[j]] = estimate_y[[j]]$b0
+        b[[j]] = estimate_y[[j]]$b1
+        x[[j]] = estimate_y[[j]]$x_i
+        mu[[j]] = estimate_y[[j]]$mu
+        th[[j]] = estimate_y[[j]]$th
+        p[j] = estimate_y[[j]]$p
+    }
+
+  temp <- do.call(rbind,
+                  mclapply(1:k,function(t)
+                  {
+                      .C("gibbs", meanz = as.double(mean_x[t,]),
+                         lastz = as.double(update_y[t,]),
+                         as.integer(nburnin),
+                         as.integer(ndraw),
+                         as.double(a[[1]]),
+                         as.double(b[[1]]),
+                         as.integer(x[[1]][t,]),
+                         as.double(mu[[1]][t,]),
+                         as.double(th[[1]][1,]),
+                         as.integer(p[1]),
+                         as.double(a[[2]]),
+                         as.double(b[[2]]),
+                         as.integer(x[[2]][t,]),
+                         as.double(mu[[2]][t,]),
+                         as.double(th[[2]][1,]),
+                         as.integer(p[2]),
+                         as.integer(r),
+                         as.double(sdev),
+                         as.integer(n),PACKAGE = 'SMGR')[[1]]
+                  }
+              , mc.cores = detectCores(), mc.preschedule = FALSE, mc.set.seed = FALSE))
+  return (temp)}
+
+
+
